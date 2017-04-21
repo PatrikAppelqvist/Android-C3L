286b91c50d841a81099ccc93fbeea8ab8661f091
Patrik Appelqvist <patrik.appelqvist@plotagon.com>
2017-04-21 12:47:17 +0200

    Added more views to recyclerview and fixed scrolling bug

--- week1/day3/code/MyApplication/app/src/main/java/se/paap/recyclerview/MainActivity.java
+++ week1/day3/code/MyApplication/app/src/main/java/se/paap/recyclerview/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         List<String> strings = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 1000; i++) {
             strings.add("Hejsan " + i);
         }
 
