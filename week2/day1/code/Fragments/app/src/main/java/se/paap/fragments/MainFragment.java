3f5161d08398db757e4ecfe2ec891627c20c39cf
Patrik Appelqvist <patrik.appelqvist@plotagon.com>
2017-04-26 09:20:21 +0200

    Don't do this

---
+++ week2/day1/code/Fragments/app/src/main/java/se/paap/fragments/MainFragment.java
@@ -0,0 +1,21 @@
+package se.paap.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by patrik on 2017-04-26.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
